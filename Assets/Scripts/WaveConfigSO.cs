0a9823ae8e3bc055a36e20deb67c3dc01752a329
matthewdozeman <mado175@student.oaisd.org>
2023-04-20 21:38:32 -0400

    AOEU

--- Assets/Scripts/WaveConfigSO.cs
+++ Assets/Scripts/WaveConfigSO.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 [CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")] 
 public class WaveConfigSO : ScriptableObject
 {
@@ -11,22 +10,18 @@ public class WaveConfigSO : ScriptableObject
     [SerializeField] float timeBetweenEnemySpawns = 1f;
     [SerializeField] float spawnTimeVariance = 0f;
     [SerializeField] float minimumSpawnTime = 0.2f;
-
     public int GetEnemyCount()
     {
         return enemyPrefabs.Count;
     }
-
     public GameObject GetEnemyPrefab(int index)
     {
         return enemyPrefabs[index];
     }
-
     public Transform GetStartingWaypoint()
     {
         return pathPrefab.GetChild(0);
     }
-
     public List<Transform> GetWaypoints()
     {
         List<Transform> waypoints = new List<Transform>();
@@ -36,12 +31,10 @@ public class WaveConfigSO : ScriptableObject
         }
         return waypoints;
     }
-
     public float GetMoveSpeed()
     {
         return moveSpeed;
     }
-
     public float GetRandomSpawnTime()
     {
         float spawnTime = Random.Range(timeBetweenEnemySpawns - spawnTimeVariance,
